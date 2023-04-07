14c4cda30bdd1018fc57ec10b9c19ca1534028d8
Анна <a.marshalova@g.nsu.ru>
2023-04-07 18:03:11 +0700

    data loader created

---
+++ data_loader.py
@@ -0,0 +1,103 @@
+import os
+import csv
+from tqdm.notebook import tqdm
+from typing import Tuple, List
+from utils import ASPECTS_LIST, flatten, paths
+
+
+class DataLoader:
+
+    def __init__(self, path_to_files: str = paths['data'], max_len: int = 100):
+        """
+        :param path_to_files: Путь к файлам с разметкой
+        :param max_len: Максимальное количество токенов в одном тексте. Тексты, содержащие большее число токенов, делятся на несколько частей.
+        """
+        self._path_to_files = path_to_files
+        self._max_len = max_len
+
+    def process_label(self, label: str) -> str:
+        """
+        Удаление аспектов, которые есть в датасете, но не участвуют в экспериментах
+        :param label: Метка класса из датасета
+        :return: Мета класса, состоящая только из тех аспектов, которые участвуют в экспериментах
+        """
+        tags = label.split('|')
+        processed_label = [tag for tag in tags if tag in ASPECTS_LIST]
+        if processed_label:
+            return '|'.join(processed_label)
+        else:
+            return 'O'
+
+    def process_file(self, file_path: str) -> Tuple[List, List, List]:
+        """
+          Загрузка одного файла с тектом. Если текст содержит большее заданного числа токенов, он делится на несколько частей.
+          :param file_path: Путь к файлу
+          :return: Части текста и метки классов
+          """
+        with open(file_path, 'r', encoding='utf-8') as f:
+            text_parts, text_part_labels = [], []
+            tokens, token_labels = [], []
+            sent, sent_labels = [], []
+            reader = csv.DictReader(f)
+            for row in reader:
+                sent.append(row['token'])
+                sent_labels.append(self.process_label(row['tag']))
+                if row['token'] == '.':
+                    if len(tokens) + len(sent) > self._max_len:
+                        text_parts.append(tokens)
+                        text_part_labels.append(token_labels)
+                        tokens = []
+                        token_labels = []
+                    tokens.extend(sent)
+                    token_labels.extend(sent_labels)
+                    sent = []
+                    sent_labels = []
+            if len(tokens) > 0:
+                text_parts.append(tokens)
+                text_part_labels.append(token_labels)
+            return text_parts, text_part_labels
+
+    def load_dir(self, dir_path: str) -> Tuple[List[List], List[List]]:
+        """
+          Загрузка текстов из одной папки.
+          :param dir_path: Путь к папке
+          :return: Тексты и метки классов
+          """
+        samples = []
+        labels = []
+        filenames = sorted(os.listdir(dir_path))
+        for filename in filenames:
+            if filename.startswith('.'):
+                continue
+            file_path = os.path.join(dir_path, filename)
+            text_parts, text_part_labels = self.process_file(file_path)
+            samples.extend(text_parts)
+            labels.extend(text_part_labels)
+        return samples, labels
+
+    def load_dataset(self, mode='cross_domain_flat') -> Tuple[List, List, List]:
+        """
+        Загрузка датасета
+        :param mode: {'flat', 'cross_domain_flat','cross_domain'}
+            ``'flat'``: Тексты загружаются из одной папки
+            ``'cross_domain_flat'``: Тексты загружаются из разых папок, но сохраняются в одном списке
+            ``'cross_domain'``: Тексты загружаются из разых папок и сохраняются в разных списках
+        :return: Тексты, метки классов и список доменов
+        """
+        if mode.startswith('cross_domain'):
+            dataset_samples = []
+            dataset_labels = []
+            domains = sorted(os.listdir(self._path_to_files))
+            for domain in tqdm(domains, desc='loading dataset'):
+                domain_path = os.path.join(self._path_to_files, domain)
+                samples, labels = self.load_dir(domain_path)
+                dataset_samples.append(samples)
+                dataset_labels.append(labels)
+            if mode == 'cross_domain_flat':
+                return flatten(dataset_samples), flatten(
+                    dataset_labels), domains  # mode = 'cross_domain_flat' Tuple[List[List], List[List], List] Списки токенов в списках текстов
+            return flatten(dataset_samples), flatten(
+                dataset_labels), domains  # mode = 'cross_domain' Tuple[List[List[List]], List[List[List]], List] Списки токенов в списках текстов в списках доменов
+        dataset_samples, dataset_labels = self.load_dir(self._path_to_files)
+        domains = ['']
+        return dataset_samples, dataset_labels, domains  # mode = 'flat' Tuple[List[List], List[List], ['']] Списки токенов в списках текстов
---
+++ utils.py
@@ -0,0 +1,10 @@
+import os
+from typing import List, Any
+
+def flatten(nested_list:List[List[Any]]) -> List[Any]:
+  return [item for nest in nested_list for item in nest]
+
+ASPECTS_LIST=['Task', 'Contrib', 'Method','Conc']
+ROOT = '/content/drive/MyDrive/Маршалова Аня'
+paths = {'data': os.path.join(ROOT,'датасеты','cross_domain_bio_aspects')}
+
